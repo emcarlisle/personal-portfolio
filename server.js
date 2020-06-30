81286797e10ab42cab14327d8aab98bef71aa3b5
Emily Carlisle <emilydeluna96@gmail.com>
2020-06-29 21:30:19 -0500

    editing server.js

--- server.js
+++ server.js
@@ -1,53 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const Handlebars = require('handlebars');
-const exphbs = require('express-handlebars');
-const {
-  allowInsecurePrototypeAccess
-} = require('@handlebars/allow-prototype-access');
-const morgan = require('morgan');
-const routes = require('./routes');
-const db = require('./models');
+// *****************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+//
+// ******************************************************************************
+// *** Dependencies
+// =============================================================
+var express = require("express");
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
 
-// Middleware
-app.use(express.urlencoded({ extended: false }));
+// Requiring our models for syncing
+var db = require("./models");
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
-app.use(morgan('dev'));
 
-// Handlebars
-app.engine(
-  'handlebars',
-  exphbs({
-    defaultLayout: 'main',
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
-  })
-);
-app.set('view engine', 'handlebars');
+// Static directory
+app.use(express.static("public"));
 
 // Routes
-app.use(routes);
+// =============================================================
+require("./routes/html-routes.js")(app);
+require("./routes/apiRoutes.js")(app);
 
-const syncOptions = { force: false };
-
-// If running a test, set syncOptions.force to true
-// clearing the `testdb`
-if (process.env.NODE_ENV === 'test') {
-  syncOptions.force = true;
-}
-
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(() => {
-  app.listen(PORT, () => {
-    console.log(
-      '==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.',
-      PORT,
-      PORT
-    );
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync({ force: true }).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
   });
 });
-
-module.exports = app;
