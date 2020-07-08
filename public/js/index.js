5152666f246c4e9fcef7fb1b4108dac5880eddf8
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-08 00:36:03 -0500

    fixing changes

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
     $("#more-btn").on("click", function () {
         $("more-pg").removeClass("d-none");
-        $("#nav").addClass("d-none");
-        $("#home-pg").addClass("d-none");
-        $("#portfolio-pg").addClass("d-none");
-        $("#contact-me").addClass("d-none");
     })
     $("#back-btn").on("click", function () {
         $("more-pg").addClass("d-none");
+        $("#nav").removeClass("d-none");
+        $("#home-pg").removeClass("d-none");
+        $("#portfolio-pg").removeClass("d-none");
+        $("#contact-me").removeClass("d-none");
 
     })
 })
