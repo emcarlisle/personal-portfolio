b279574706bd39ff79f75373d7223bc6a6868321
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-08 00:09:23 -0500

    removing files, fixing render paths

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+    $("#more-btn").on("click", function () {
+        $("more-pg").removeClass("d-none");
+        $("#nav").addClass("d-none");
+        $("#home-pg").addClass("d-none");
+        $("#portfolio-pg").addClass("d-none");
+        $("#contact-me").addClass("d-none");
+    })
+    $("#back-btn").on("click", function () {
+        $("more-pg").addClass("d-none");
+
+    })
 })
