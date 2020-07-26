a0bddafe2ff0b03c00908f58551b298f3849bead
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-26 18:49:15 -0500

    initial react files, app.js and index.js

---
+++ personal-portfolio/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import More from "./components/Pages/More";
+import About from "./components/Pages/About";
+import Contact from "./components/Pages/Contact";
+import Portfolio from "./components/Pages/Portfolio";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+//import Project from "./components/Project";
+
+// returns the Container components that have the navbar, table components and API call
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Route exact path="/" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/portfolio" component={Portfolio} />
+        <Route exact path="/more" component={More} />
+        <Footer />
+      </div>
+    </Router>
+  )
+}
+
+export default App;
