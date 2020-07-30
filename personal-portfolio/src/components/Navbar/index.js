4320e9c5b1a254ecdac5e0841570a78003c0cbc4
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-29 21:55:49 -0500

    flex-row nav

--- personal-portfolio/src/components/Navbar/index.js
+++ personal-portfolio/src/components/Navbar/index.js
@@ -2,74 +2,74 @@ import React from "react";
 import { Link } from "react-router-dom";
 import sunset from "./sunset.jpg";
 
-
 function Navbar() {
   return (
     <div className="container-fluid" id="nav">
-      <section className="jumbotron jumbotron-fluid" style={{backgroundImage: `url(${sunset})`}}>
+      <section
+        className="jumbotron jumbotron-fluid"
+        style={{ backgroundImage: `url(${sunset})` }}
+      >
         <div className="container">
           <h1 className="display-4">I am Emily Carlisle</h1>
-          <p className="lead">Full Stack Web Developer</p>
+          <p className="lead">Junior Full Stack Web Developer</p>
         </div>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div>
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link
-                  to="/"
-                  className={
-                    window.location.pathname === "/" ||
-                      window.location.pathname === "/about"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/contact"
-                  className={
-                    window.location.pathname === "/contact"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/portfolio"
-                  className={
-                    window.location.pathname === "/portfolio"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Portfolio
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  to="/more"
-                  className={
-                    window.location.pathname === "/more"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  More
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+        <div>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                to="/"
+                className={
+                  window.location.pathname === "/" ||
+                  window.location.pathname === "/about"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contact"
+                className={
+                  window.location.pathname === "/contact"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/portfolio"
+                className={
+                  window.location.pathname === "/portfolio"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/more"
+                className={
+                  window.location.pathname === "/more"
+                    ? "nav-link active"
+                    : "nav-link"
+                }
+              >
+                More
+              </Link>
+            </li>
+          </ul>
+        </div>
+
       </section>
     </div>
   );
 }
-
 
 export default Navbar;
